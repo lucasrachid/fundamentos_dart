2c34926c94003b0b3877b0027a0e1dd6fc296c76
Lucas Rachid Martins <lucasrachid@hotmail.com>
2023-02-24 20:58:10 -0300

    Batata

--- bin/fundamentos_dart.dart
+++ bin/fundamentos_dart.dart
@@ -2,9 +2,9 @@ import 'package:fundamentos_dart/fundamentos_dart.dart' as fundamentos_dart;
 import 'dart:io';
 
 void main(List<String> arguments) {
-  fundamentos_dart.mostrarInfos();
-  fundamentos_dart.mostrarInformacoesProduto();
-  String? inputNome;
-  print('Informe o seu nome: ${inputNome = stdin.readLineSync()}');
+  // fundamentos_dart.mostrarInfos();
+  // fundamentos_dart.mostrarInformacoesProduto();
+  print('Informe o seu nome: ');
+  String? inputNome = stdin.readLineSync();  
   print('Bem vindo ${inputNome}');
 }
